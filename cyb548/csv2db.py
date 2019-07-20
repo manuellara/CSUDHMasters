ba7e57d8cdd481cc04cc587bc824dca4a3c48500
ManuelLara <manuellaraa@gmail.com>
2019-07-19 23:54:32 -0700

    update

--- cyb548/csv2db.py
+++ cyb548/csv2db.py
@@ -1,152 +1,96 @@
-import pandas as pd 
+import pandas as pd
 import sqlite3
+import os
 
-#gets index of last row => returns index of last row
-def getIndexOfLastRow( df ):
-        indexOfLastRow = df.tail(1).index.item()
-        
-        return indexOfLastRow
+class csv2db:
+    def __init__(self, dbName, csv):
+        self.dbName = dbName
+        self.csv = csv
 
-#returns dataframe object from CSV
-def retrieveCSV():
-        #loads gun violence csv file
-        df = pd.read_csv('gun-violence-data_01-2013_03-2018.csv')
-        
-        return df
-
-#gets column headers => returns headers as pandas object 
-def getColumnHeaders( df ):
-        #gets column headers 
-        columnHeaders = df.columns 
-
-        print("\n[ ✔ ] CSV coulmn headers saved")
-        
-        return columnHeaders
-
-#builds SQL statemanet off column headers
-def constructSQLstatement( columnHeaders ):
-        tableName = "gunViolenceData"
-        
-        templateSQL = "CREATE TABLE ! ( ? )"
-        templateString = "field text"
-        placeholder = ''
-
-        placeholder = ', '.join( str( templateString.replace( "field" , i ) ) for i in columnHeaders )
-
-        templateSQL = templateSQL.replace( "?", placeholder )
-        templateSQL = templateSQL.replace( "!", tableName )
-
-        return templateSQL, tableName
-
-#creates database, table, and fields 
-def initializeDB( columnHeaders ):
-        #DB name
-        dbName = 'data.db'
-
-        #initializes database OR connects to it if it already exits 
-        conn = sqlite3.connect( dbName )
-
-        print(f"\n[ ✔ ] Connected to {dbName}")
-
-        #creates cursor to navigate database 
-        c = conn.cursor()
-
-        #try to create table => close DB when done
+    def getDataFrame(self):
         try:
-                sqlStatement, tableName = constructSQLstatement( columnHeaders )
-
-                #create fields in table & fields 
-                c.execute( sqlStatement ) 
-
-                #commits data to database 
-                conn.commit()
-
-                print(f"[ ✔ ] Table '{tableName}' has been created")
+            self.df = pd.read_csv(self.csv)
         except Exception as e:
-                print("[ × ]", e)
+            print("[ × ] Failed getting data frame: ", e)
         finally:
-                #closes database connection
-                conn.close()
+            print("[ ✔ ] CSV converted to dataframe")
 
-                print("[ ✔ ] Database connection closed")
-                
-        return dbName, tableName
-
-#builds SQL insert stament => returns insert statement 
-def constructSQLInsertStatement( df , index , tableName ):
-        tableName = tableName
-
-        templateSQL = "INSERT INTO ! VALUES ( ? )"
-        templateString = "field"
-        placeholder = ''
-
-        placeholder = ', '.join( '"' + str( templateString.replace( "field" , str( x ) )  ).replace( '"', '`' ) + '"' for x in df.iloc[index] )
-
-        templateSQL = templateSQL.replace( "?", placeholder )
-        templateSQL = templateSQL.replace( "!", tableName )
-
-        return templateSQL
-
-#takes db name and 
-def populateDB( dbName , df , tableName ):
+    def getHeaders(self):
         try:
-                #initialize counter
-                x = 0
-
-                #initializes database OR connects to it if it already exits 
-                conn = sqlite3.connect( dbName )
-
-                #creates cursor to navigate database 
-                c = conn.cursor()
-
-                print(f"\n[ ✔ ] Connected to {dbName}")
-                print( "[ ✔ ] Inserting rows in progress..." )
-
-                for i in range( 0 , getIndexOfLastRow(df) + 1 ):
-                        #calls insert statement function 
-                        insertStatement = constructSQLInsertStatement( df , i , tableName )
-                        
-                        #create fields in table & fields 
-                        c.execute( insertStatement ) 
-
-                        #increment counter
-                        x += 1
-                
-                #commits data to database 
-                conn.commit()
-
-                if x > 1:
-                        row = "rows"
-                else:
-                        row = "row"
-
-                print( f"[ ✔ ] Successfully inserted {x} {row} into the {tableName} table" )
+            self.headers = self.df.columns
+            print("[ ✔ ] Headers saved")
         except Exception as e:
-                print("[ × ]", e)
-        finally:
-                #closes database connection
-                conn.close()
+            print("[ × ] Failed getting headers: ", e)
 
-                print("[ ✔ ] Database connection closed")
-        
-        return
+    def createDatabase(self):
+        try:
+            if not os.path.exists(self.dbName):
+                self.conn = sqlite3.connect(self.dbName)
+                self.c = self.conn.cursor()
+                print("[ ✔ ] Database created & connected")
+            else:
+                self.conn = sqlite3.connect(self.dbName)
+                self.c = self.conn.cursor()
+                print("[ ✔ ] Database connected")
+        except Exception as e:
+            print("[ × ] Failed creating databse: ", e)
 
+    def getTableName(self):
+        self.tableName = "gunViolenceData"
 
+    def insertHeaders(self):
+        self.templateSQL = "CREATE TABLE ! ( ? )"
+        self.templateString = "field text"
+        self.placeholder = ''
+        try:
+            self.placeholder = ', '.join(
+                str(self.templateString.replace("field", i)) for i in self.headers)
+            self.templateSQL = self.templateSQL.replace("?", self.placeholder)
+            self.templateSQL = self.templateSQL.replace("!", self.tableName)
+        except Exception as e:
+            print(f"[ × ] Failed inserting headers into {self.tableName}: ", e)
+        else:
+            self.c.execute(self.templateSQL)
+            print(f"[ ✔ ] Headers inserted into the {self.tableName} table")
 
-##################################################################################################################
+    def getIndexOfLastRow(self):
+        self.indexOfLastRow = self.df.tail(1).index.item()
 
-print("[ ! ] START: importing CSV into DB")
+    def populateDB(self):
+        print("[ ✔ ] Inserting rows in progress...")
+        try:
+            for i in range(0, self.indexOfLastRow + 1):
+                self.templateSQL = "INSERT INTO ! VALUES ( ? )"
+                self.templateString = "field"
+                self.placeholder = ''
+                self.placeholder = ', '.join('"' + str(self.templateString.replace(
+                    "field", str(x))).replace('"', '`') + '"' for x in self.df.iloc[i])
+                self.templateSQL = self.templateSQL.replace(
+                    "?", self.placeholder)
+                self.templateSQL = self.templateSQL.replace(
+                    "!", self.tableName)
+                self.c.execute(self.templateSQL)
+            print(
+                f"[ ✔ ] {self.indexOfLastRow + 1} rows inserted into the {self.tableName} table")
+        except Exception as e:
+            print("[ × ] Failed populating database: ", e)
+        else:
+            self.conn.close()
+            print("[ ✔ ] Database connection closed")
 
-#gets CSV file we will use => returns dataframe 
-df = retrieveCSV()
+    def execute(self):
+        self.getDataFrame()
+        self.getHeaders()
+        self.createDatabase()
+        self.getTableName()
+        self.insertHeaders()
+        self.getIndexOfLastRow()
+        self.populateDB()
 
-#=> returns pandas column headers object
-columnHeaders = getColumnHeaders( df )
+databaseName = 'data.db'
+csv = 'gun-violence-data_01-2013_03-2018.csv'
 
-#connects to DB, creates table & fields => returns DB name
-dbName, tableName = initializeDB( columnHeaders ) 
+data = csv2db(databaseName, csv)
+data.execute()
 
-#add CSV data to DB
-populateDB( dbName , df , tableName )
-
-print("\n[ ! ] END: CSV successfully imported into DB")
+os.system('rm data.db')
