f44e78da9ec7b090a80b296090caf8554a02039e
ManuelLara <manuellaraa@gmail.com>
2019-08-02 00:07:19 -0700

    update

--- cyb548/sheets.py
+++ cyb548/sheets.py
@@ -3,24 +3,65 @@ from oauth2client.service_account import ServiceAccountCredentials
 import sqlite3 
 
 
-def writeDBHeaders():
-    dbName = "data.db"
-    tableName = "gunViolenceData"
 
-    conn = sqlite3.connect(dbName)
-    c = conn.cursor()
+class db2sheets:
+    def __init__(self, creds , dbName , tableName , sheetName):
+        self.scope = ['https://www.googleapis.com/auth/drive']
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(creds , self.scope) 
+        self.client = gspread.authorize(self.credentials)
+        self.dbName = dbName
+        self.tableName = tableName 
+        self.sheetName = sheetName
 
-    sql = f"select * from {tableName} where 1=0;"
+    def connectToDB(self):
+        try:
+            self.conn = sqlite3.connect(self.dbName)
+            self.c = self.conn.cursor()    
+            print(f"[ ✔ ] Connected to {self.sheetName}")
+        except Exception as e:
+            print(f"[ × ] Failed connecting to {self.dbName}: ", e)
 
-    c.execute(sql)
+    def connectToSheet(self):
+        try:
+            self.sheet = self.client.open('projectMgmt').sheet1
+            print(f"[ ✔ ] Connected to {self.sheetName}")
+        except Exception as e:
+            print(f"[ × ] Failed connecting to {self.sheetName}: ", e)
 
-    headers = [d[0] for d in c.description]
+    def writeDBHeaders(self):
+        try:
+            self.sql = f"select * from {self.tableName} where 1=0;"
+            self.c.execute(self.sql)
+            self.headers = [d[0] for d in self.c.description]
+            self.sheet.append_row( self.headers )
+            print(f"[ ✔ ] Headers successfully written to {self.sheetName}")
+        except Exception as e:
+            print(f"[ × ] Failed getting/writing headers: ", e)
 
-    # writes headers to first row 
-    sheet.append_row( headers )
 
-    conn.close()
-    return
+
+
+
+
+
+# def writeDBHeaders():
+#     dbName = "data.db"
+#     tableName = "gunViolenceData"
+
+#     conn = sqlite3.connect(dbName)
+#     c = conn.cursor()
+
+#     sql = f"select * from {tableName} where 1=0;"
+
+#     c.execute(sql)
+
+#     headers = [d[0] for d in c.description]
+
+#     # writes headers to first row 
+#     sheet.append_row( headers )
+
+#     conn.close()
+#     return
 
 
 def populateSheets():
@@ -44,17 +85,27 @@ def populateSheets():
 
 
 
-scope = ['https://www.googleapis.com/auth/drive']
+# scope = ['https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope) 
+# credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope) 
 
-client = gspread.authorize(credentials)  
+# client = gspread.authorize(credentials)  
 
 #connects to 'projectMgmt' sheet
-sheet = client.open('projectMgmt').sheet1
+# sheet = client.open('projectMgmt').sheet1
 
 
 # gets db headers 
 writeDBHeaders()
 
 populateSheets()
+
+
+
+creds = "creds.json"
+dbName = "data.db" 
+tableName = "gunViolenceData"
+sheetName = "projectMgmt"
+
+
+driver = db2sheets( creds , dbName , tableName , sheetName )
